0245f3fd7f6b139e80a658a63cfb2618ec5db025
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-01-09 11:59:07 +0600

    deleted unncessary files

--- TestCodes/GUI-Practice/EmptyGui.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-
-app = QApplication(sys.argv)  # 2. Create QApplication object
-window = QWidget()  # 3. Create window from QWidget object
-window.show()  # 4. Call show to display GUI window
-# 5. Start the event loop. Use sys.exit to close the program
-sys.exit(app.exec())
