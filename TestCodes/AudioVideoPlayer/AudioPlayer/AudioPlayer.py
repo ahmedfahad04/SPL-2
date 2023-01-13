9934c607446aea5147a989f7ab85ea978e1e53f2
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2023-01-13 22:03:53 +0600

    Video Player issue solved with codec file

---
+++ TestCodes/AudioVideoPlayer/AudioPlayer/AudioPlayer.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+pygame.mixer.init()  # Initialize the mixer module.
+sound1 = pygame.mixer.Sound('musics/FreeMP3.mp3')  # Load a sound.
+
+while True:
+    try:
+        inpt = input('Press enter to play the sound: ')
+        sound1.play()  # Play the sound.
+        print('Playing sound')
+    except KeyboardInterrupt:
+        break
--- TestCodes/AudioVideoPlayer/VideoPlayer.py
+++ TestCodes/AudioVideoPlayer/VideoPlayer.py
@@ -5,13 +5,13 @@ from PyQt5 import QtWidgets, QtMultimedia, uic, QtCore
 from PyQt5.QtCore import QUrl
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 
-
 class Form(QtWidgets.QDialog):
     def __init__(self, parent=None):
         QtWidgets.QDialog.__init__(self, parent)
+        self.setWindowTitle("Demo Media Player")
         self.ui = uic.loadUi(os.path.join(os.path.dirname(__file__), "form.ui"), self)
         self.player = QtMultimedia.QMediaPlayer(None, QtMultimedia.QMediaPlayer.VideoSurface)
-        file = os.path.join(os.path.dirname(__file__), "small.mp4")
+        file = os.path.join(os.path.dirname(__file__), "VideoPlayer/small.mp4")
         self.player.setMedia(QtMultimedia.QMediaContent(QtCore.QUrl.fromLocalFile(file)))
         self.player.setVideoOutput(self.ui.widget)
         self.player.play()
