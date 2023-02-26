d173ccfd0a53e10d75576707777afdd27c03c2b5
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-02-26 19:25:40 +0600

    video capturing with open cv

---
+++ TestCodes/VideoRecording/video_recorder.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+writer = cv2.VideoWriter('basic-video.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+
+while True:
+    ret, frame = cap.read()
+
+    writer.write(frame)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
