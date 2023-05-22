817067828b6bcbb993d34047cf4e8d477794c7e2
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-05-22 18:31:53 +0600

    splash screen added | and audio device problem solved (maybe)

---
+++ EmPower/Student/Frontend/src/SplashScreen.py
@@ -0,0 +1,41 @@
+import time
+
+from PyQt5.QtWidgets import QSplashScreen
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
+
+
+class SplashScreen(QSplashScreen):
+    def __init__(self):
+        super(QSplashScreen, self).__init__()
+        loadUi("Frontend/PyQT_UI/splash.ui", self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setWindowOpacity(0.8)
+        # Center the splash screen window on the screen using style commands
+        self.setStyleSheet('''
+           QSplashScreen {
+               background-color: rgba(255,228,196, 0.8);
+               background-position: center;
+           }
+       ''')
+
+        # Customize the progress bar
+        self.progressBar.setStyleSheet('''
+            QProgressBar {
+                border: 2px solid grey;
+                border-radius: 5px;
+                background-color: #f0f0f0;
+                text-align: center;
+                position: center;
+            }
+
+            QProgressBar::chunk {
+                background-color: rgb(255,127,80);  
+                width: 8px;
+            }
+        ''')
+
+    def progress(self):
+        for i in range(100):
+            time.sleep(0.05)
+            self.progressBar.setValue(i)
