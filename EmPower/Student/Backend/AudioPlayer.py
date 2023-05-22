fe49bebe4edf347f524cd5b1837d014b8b646e11
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-05-22 13:49:03 +0600

    audio device problem solved

--- EmPower/Student/Backend/AudioPlayer.py
+++ EmPower/Student/Backend/AudioPlayer.py
@@ -3,20 +3,35 @@ import threading
 import pygame
 import time
 
+
 class MusicPlayer:
-    
+
     def __init__(self, music_file_path):
         self.music_file_path = music_file_path
         self.play_music_continuously = False
         self.music_thread = None
-        pygame.mixer.init()
+        if self.check_audio_devices():
+            print("audio device found")
+            pygame.mixer.init()
+        else:
+            print("no audio device")
+
+    def check_audio_devices(self):
+        pygame.mixer.get_init()
+        try:
+            pygame.mixer.get_num_channels()
+            print("Audio output device found.")
+            return True
+        except pygame.error:
+            print("No audio output device found. Connect an audio device and try again.")
+            return False
 
     def play_music(self):
         pygame.mixer.init()
-        
+
         self.play_music_continuously = True
         pygame.mixer.music.load(self.music_file_path)
-   
+
         try:
             while self.play_music_continuously:
                 pygame.mixer.init()
@@ -30,7 +45,6 @@ class MusicPlayer:
                 time.sleep(2)
         except Exception as e:
             print(e)
-        
 
     def start_music(self):
         self.music_thread = threading.Thread(target=self.play_music, daemon=True)
